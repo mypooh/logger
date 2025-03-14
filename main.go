0d3149b51a2e7d1056685455014b5669c0d72d80
mypooh <HannahMcLean2002@proton.me>
2025-03-14 23:37:31 +0900

    add function name log feature

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ type customLogWriter struct {
 func (w *customLogWriter) Write(p []byte) (n int, err error) {
 	// Write other messages to the original writer
 	if w.fileWriter != nil {
+		w.fileWriter.Seek(0, 2)
 		w.fileWriter.Write(p)
 		w.fileWriter.Sync()
 	}
@@ -45,5 +46,30 @@ func LogString(filename string, success bool, depth int, format string, value ..
 	}
 	logger := log.New(logWriter, prefix, log.LstdFlags)
 	pc, _, line, _ := runtime.Caller(depth)
-	logger.Printf("Function: %s, Line: %d, Message: %s", runtime.FuncForPC(pc).Name(), line, fmt.Sprintf(format, value...))
+	logger.Printf("Function: %s, Line: %d, Message: %s\n", runtime.FuncForPC(pc).Name(), line, fmt.Sprintf(format, value...))
+}
+
+func FullLogString(filename string, success bool, depth int, format string, value ...any) {
+	var logFile *os.File
+	var err error
+	if filename != "" {
+		logFile, err = os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0644)
+	}
+	if logFile != nil {
+		defer logFile.Close()
+	}
+	logWriter := &customLogWriter{}
+	if err != nil {
+		logWriter.fileWriter = nil
+	} else {
+		logWriter.fileWriter = logFile
+	}
+	logWriter.consoleWriter = os.Stdout
+	prefix := "[+]"
+	if !success {
+		prefix = "[-]"
+	}
+	logger := log.New(logWriter, prefix, log.LstdFlags)
+	pc, _, line, _ := runtime.Caller(depth)
+	logger.Printf("Function: %s, Line: %d, Message: %s\n", runtime.FuncForPC(pc).Name(), line, fmt.Sprintf(format, value...))
 }
